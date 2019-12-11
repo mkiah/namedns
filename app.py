4f1ef860dd2416f69d17ffbdcfd9ff9c5f796809
Mitch Kiah <mitch@mitchkiah.com>
2019-12-10 21:45:52 -0800

    Added argparse for command line support

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+import argparse
 from utils import namedns
 
-namedotcom = namedns.Session('user', 'token', True)
 
-namedotcom.list('pseudo.coffee')
+def main():
+    parser = argparse.ArgumentParser(description="Generate or renew letsencrypt \
+        certificates and authenticate with name.com API")
+    required_args = parser.add_argument_group('required arguments')
+    required_args.add_argument('-u', '--username', dest='username', type=str, required=True, help='Name.com username.')
+    required_args.add_argument('-t', '--api-token', dest='api_token', type=str, required=True, help='Name.com password.')
+    parser.add_argument('-d', '--dev', dest='dev', action='store_true', help='Use the name.com dev server.')
+    args = parser.parse_args()
+
+    print('Ignoring --dev flag and hardcoding to user dev server for testing.')
+    namedotcom = namedns.Session(args.username, args.api_token, True) #Forced to use DEV environment while testing
+    namedotcom.list('pseudo.coffee')
+
+if __name__ == '__main__':
+    main()
