385c7200c9f057d8cfe93128317fe8988fb0fd09
Mitch Kiah <mitch@mitchkiah.com>
2019-12-12 17:31:36 -0800

    Reorganized argparse inputs and loosened requirements.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import argparse
 import pprint
 import sys
+import getpass
 from utils import namedns
-
 
 def add_record(domain, host, record_type, ttl, answer, priority=None):
     return namedotcom.create_record(domain=domain, host=host, record_type=record_type, ttl=ttl, answer=answer, priority=priority)
@@ -14,20 +14,18 @@ def remove_record(domain, record_id):
 
 parser = argparse.ArgumentParser(description="Generate or renew letsencrypt \
                                  certificates and authenticate with name.com API")
-required = parser.add_argument_group('required arguments')
-required.add_argument('-u', '--username', dest='username',
-                      type=str, required=True, help='Name.com username.')
-required.add_argument('-a', '--api-token', dest='api_token',
-                      type=str, required=True, help='Name.com password.')
-required.add_argument('-d', '--domain', dest='domain',
-                      type=str, help='The domain to take action on.')
+parser.add_argument('--user', dest='username', type=str, help='API username.')
+parser.add_argument('--token', dest='api_token', type=str, help='API auth token.')
+parser.add_argument('--domain', dest='domain', type=str, help='The domain to take action on.')
+parser.add_argument('--dev', "--test", dest='dev',
+                    action='store_true', help='Use the name.com dev server.')
 
 manage = parser.add_argument_group('manage records')
 manage.add_argument('--record', dest='record',
                     type=str, choices=['add', 'remove'])
 manage.add_argument('--host', dest='host', type=str,
                     help='Hostname relative to the domain (e.g., blog for blog.example.com)')
-manage.add_argument('--type', dest='type', type=str, required=True,
+manage.add_argument('--type', dest='type', type=str,
                     choices=['A', 'AAAA', 'ANAME', 'CNAME', 'MX', 'NS', 'SRV', 'TXT'])
 manage.add_argument('--answer', dest='answer', type=str,
                     help="Answer is either the IP address for A or AAAA records; the \
@@ -38,22 +36,27 @@ manage.add_argument('--ttl', dest='ttl', default=3600, type=int,
                     help="TTL is the time this record can be cached for in seconds. Minimum of 300 seconds")
 manage.add_argument('--priority', dest='priority', type=int,
                     help="Priority is only required for MX and SRV records, it is ignored for all others.")
-
-parser.add_argument('--dev', "--test", dest='dev',
-                    action='store_true', help='Use the name.com dev server.')
 args = parser.parse_args()
-if args.ttl and args.ttl < 300:
-    parser.error(
-        f'Time to live (TTL) value of {args.ttl} invalid. Minimum value is 300.')
+
+args.username = args.username if args.username else input('Username: ')
+args.api_token = args.api_token if args.api_token else getpass.getpass()
 
 namedotcom = namedns.Session(args.username, args.api_token, args.dev)
 
 if args.record == 'add':
+    if not (args.type and args.host and args.answer):
+        parser.error(f'Requires --type, --host, and --answer.')
+
+    if args.ttl and args.ttl < 300:
+        parser.error(
+            f'Time to live (TTL) value of {args.ttl} invalid. Minimum value is 300.')
+
     record = add_record(domain=args.domain, host=args.host,
                         record_type=args.type, ttl=300, answer=args.answer)
     pprint.pprint(record)
 elif args.record == 'remove':
-    # raise NotImplementedError
+    if not (args.type and args.host):
+        parser.error(f'Requires --type and --host.')
 
     records = namedotcom.list_records(args.domain)
     # pprint.pprint(records)
@@ -65,3 +68,8 @@ elif args.record == 'remove':
             changed_records.append(removed)
     print(f'Removed {len(changed_records)} records from {args.domain}.')
     pprint.pprint(changed_records)
+elif args.domain:
+    pprint.pprint(namedotcom.get_domain(args.domain))
+else:
+    print(f'No tasks specified. Fetching list of associated domains.')
+    pprint.pprint(namedotcom.list_domains())
